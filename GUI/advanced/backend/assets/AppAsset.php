5d0e829f4aa93ab031b1668e8b23f5e9186e179a
Danya Balcena <dpbalcena@gmail.com>
2017-02-19 17:59:11 +0800

    Redirect frontend user to sign in when not logged in

--- GUI/advanced/backend/assets/AppAsset.php
+++ GUI/advanced/backend/assets/AppAsset.php
@@ -13,8 +13,30 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/bootstrap.css',
+        'css/bootstrap.min.css',
+        'css/bootstrap-theme.css',
+        'css/bootstrap-theme.min.css',
+        'dist/css/AdminLTE.css',
+        'dist/css/AdminLTE.min.css',
+        'dist/css/alt/AdminLTE-bootstrap-social.css',
+        'dist/css/alt/AdminLTE-bootstrap-social.min.css',
+        'dist/css/alt/AdminLTE-fullcalendar.css',
+        'dist/css/alt/AdminLTE-fullcalendar.min.css',
+        'dist/css/alt/AdminLTE-select2.css',
+        'dist/css/alt/AdminLTE-select2.min.css',
+        'dist/css/alt/AdminLTE-without-plugins.css',
+        'dist/css/alt/AdminLTE-without-plugins.min.css',
+
+
     ];
     public $js = [
+    'dist/js/app.js',
+    'dist/js/app.min.js',
+    'dist/js/demo.js',
+    'dist/js/pages/dashboard.js',
+    'dist/js/pages/dashboard.js',
+
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- GUI/advanced/backend/views/site/index.php
+++ GUI/advanced/backend/views/site/index.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'NDRRMC Monitoring System';
 ?>
 <div class="site-index">
 
