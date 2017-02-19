da836780a23654057d3c6238d796f6eb7d0571d4
airacarpio <airajoyce04@gmail.com>
2017-02-19 20:51:20 +0800

    edit form in suggested-supplies/_form.php

--- GUI/advanced/backend/views/suggested-supplies/_form.php
+++ GUI/advanced/backend/views/suggested-supplies/_form.php
@@ -12,14 +12,12 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-<<<<<<< HEAD
+    
+  <!--  <?= $form->field($model, 'id')->textInput() ?> -->
+
     <?= $form->field($model, 'disaster_type')->dropDownList(['Earthquake' => 'Earthquake', 'Flood' => 'Flood','Low Pressure Area' => 'Low Pressure Area', 'Tropical Depression' => 'Tropical Depression', 'Typhoon' => 'Typhoon','Volcanic' => 'Volcanic', 'Wind' =>'Wind', 'Other' => 'Other',], ['prompt' => '==Select==']) ?>
-=======
-    <?= $form->field($model, 'id')->textInput() ?>
 
-    <?= $form->field($model, 'disaster_type')->textInput(['maxlength' => true]) ?>
->>>>>>> effccbd9c73a9802c8e2c27d71026188ddbf4397
-
+ 
     <?= $form->field($model, 'equipment_name')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
