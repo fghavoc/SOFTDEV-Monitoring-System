60cb1c49a734f340a45295ac2824f893bd3a4fa7
Gail Haboc <fghaboc@gmail.com>
2017-03-07 04:14:44 +0800

    migrate db

--- GUI/monitoringv2/common/config/main-local.php
+++ GUI/monitoringv2/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=ndrrmc',
+            'dsn' => 'mysql:host=localhost;dbname=monitoring',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
