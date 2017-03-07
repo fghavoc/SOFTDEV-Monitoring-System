f526a39c8274f0c0fb8215245550223b547e5c87
airacarpio <airajoyce04@gmail.com>
2017-03-07 14:00:38 +0800

    Modified index.php and logistics page

--- GUI/monitoringv2/frontend/views/site/index.php
+++ GUI/monitoringv2/frontend/views/site/index.php
@@ -78,7 +78,7 @@ $this->title = 'Monitoring System';
           <!-- AREA CHART -->
           <div class="box box-primary">
             <div class="box-header with-border">
-              <h3 class="box-title">Count of Missing People By Region</h3>
+              <h3 class="box-title"></h3>
 
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
@@ -89,16 +89,16 @@ $this->title = 'Monitoring System';
             <div class="box-body">
       </div>
     <div class="row">
-        <div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d123573.11109763144!2d121.02848460467526!3d14.56144133350866!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sph!4v1488816889384" width="500" height="300" frameborder="0" style="border:0" margin-left="100px"allowfullscreen></iframe>
+        <div class ="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d123573.11109763144!2d121.02848460467526!3d14.56144133350866!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sph!4v1488816889384" width="570" height="550" frameborder="0" style="border:0; margin-left:30px; margin-bottom: 20px;" allowfullscreen></iframe>
             </div>
+          </div>
           </div>
 
           <!-- DONUT CHART -->
           <div class="box box-danger">
             <div class="box-header with-border">
               <h3 class="box-title">Average Age of Missing People by Gender</h3>
-
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                 </button>
@@ -114,29 +114,39 @@ $this->title = 'Monitoring System';
         <!-- /.col (LEFT) -->
 
         <div class="col-md-6">
-          <!-- LINE CHART -->
-          <div class="box box-info">
-            <div class="box-header with-border">
-              <h3 class="box-title">Count of Dead People by Region</h3>
 
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+        <!-- Calendar -->
+          <div class="box box-solid bg-green-gradient">
+            <div class="box-header ui-sortable-handle" style="cursor: move;">
+              <i class="fa fa-calendar"></i>
+
+              <h3 class="box-title">Calendar</h3>
+              <!-- tools box -->
+              <div class="pull-right box-tools">
+                <!-- button with a dropdown -->
+                <div class="btn-group">
+                  <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown">
+                    <i class="fa fa-bars"></i></button>
+                  <ul class="dropdown-menu pull-right" role="menu">
+                    <li><a href="#">Add new event</a></li>
+                    <li><a href="#">Clear events</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">View calendar</a></li>
+                  </ul>
+                </div>
+                <button type="button" class="btn btn-success btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
                 </button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                <button type="button" class="btn btn-success btn-sm" data-widget="remove"><i class="fa fa-times"></i>
+                </button>
               </div>
+              <!-- /. tools -->
             </div>
-            <div class="box-body">
-              <div class="chart">
-          <img src="http://i1360.photobucket.com/albums/r656/Gail_Haboc/NDRRMC%20img/DeadandMissing/dm3_zpsqpmjec06.png" height="400" width="550" />
-              </div>
+            <!-- /.box-header -->
+            <div class="box-body no-padding">
+              <!--The calendar -->
+              <div id="calendar" style="width: 100%"><div class="datepicker datepicker-inline"><div class="datepicker-days" style="display: block;"><table class="table table-condensed"><thead><tr><th class="prev" style="visibility: visible;">«</th><th colspan="5" class="datepicker-switch">March 2017</th><th class="next" style="visibility: visible;">»</th></tr><tr><th class="dow">Su</th><th class="dow">Mo</th><th class="dow">Tu</th><th class="dow">We</th><th class="dow">Th</th><th class="dow">Fr</th><th class="dow">Sa</th></tr></thead><tbody><tr><td class="old day">26</td><td class="old day">27</td><td class="old day">28</td><td class="day">1</td><td class="day">2</td><td class="day">3</td><td class="day">4</td></tr><tr><td class="day">5</td><td class="day">6</td><td class="day">7</td><td class="day">8</td><td class="day">9</td><td class="day">10</td><td class="day">11</td></tr><tr><td class="day">12</td><td class="day">13</td><td class="day">14</td><td class="day">15</td><td class="day">16</td><td class="day">17</td><td class="day">18</td></tr><tr><td class="day">19</td><td class="day">20</td><td class="day">21</td><td class="day">22</td><td class="day">23</td><td class="day">24</td><td class="day">25</td></tr><tr><td class="day">26</td><td class="day">27</td><td class="day">28</td><td class="day">29</td><td class="day">30</td><td class="day">31</td><td class="new day">1</td></tr><tr><td class="new day">2</td><td class="new day">3</td><td class="new day">4</td><td class="new day">5</td><td class="new day">6</td><td class="new day">7</td><td class="new day">8</td></tr></tbody><tfoot><tr><th colspan="7" class="today" style="display: none;">Today</th></tr><tr><th colspan="7" class="clear" style="display: none;">Clear</th></tr></tfoot></table></div><div class="datepicker-months" style="display: none;"><table class="table table-condensed"><thead><tr><th class="prev" style="visibility: visible;">«</th><th colspan="5" class="datepicker-switch">2017</th><th class="next" style="visibility: visible;">»</th></tr></thead><tbody><tr><td colspan="7"><span class="month">Jan</span><span class="month">Feb</span><span class="month">Mar</span><span class="month">Apr</span><span class="month">May</span><span class="month">Jun</span><span class="month">Jul</span><span class="month">Aug</span><span class="month">Sep</span><span class="month">Oct</span><span class="month">Nov</span><span class="month">Dec</span></td></tr></tbody><tfoot><tr><th colspan="7" class="today" style="display: none;">Today</th></tr><tr><th colspan="7" class="clear" style="display: none;">Clear</th></tr></tfoot></table></div><div class="datepicker-years" style="display: none;"><table class="table table-condensed"><thead><tr><th class="prev" style="visibility: visible;">«</th><th colspan="5" class="datepicker-switch">2010-2019</th><th class="next" style="visibility: visible;">»</th></tr></thead><tbody><tr><td colspan="7"><span class="year old">2009</span><span class="year">2010</span><span class="year">2011</span><span class="year">2012</span><span class="year">2013</span><span class="year">2014</span><span class="year">2015</span><span class="year">2016</span><span class="year">2017</span><span class="year">2018</span><span class="year">2019</span><span class="year new">2020</span></td></tr></tbody><tfoot><tr><th colspan="7" class="today" style="display: none;">Today</th></tr><tr><th colspan="7" class="clear" style="display: none;">Clear</th></tr></tfoot></table></div></div></div>
             </div>
-            <!-- /.box-body -->
-          </div>
-        </div>
-
-        <!-- Begin of mycalendar.org script --> 
-      x
-        <!-- End of mycalendar.org script -->
+           
     </div>
         </section>
         <!-- right col -->
