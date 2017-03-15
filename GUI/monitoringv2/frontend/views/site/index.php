f4e01de29102f605b49bd54ef3e17becea81e90f
airacarpio <airajoyce04@gmail.com>
2017-03-15 21:35:10 +0800

    Pop up page

--- GUI/monitoringv2/frontend/views/site/index.php
+++ GUI/monitoringv2/frontend/views/site/index.php
@@ -5,116 +5,283 @@
 $this->title = 'Monitoring System';
 ?>
 <div class="site-index">
-<div class="time">
-<div style="margin: 15px 0px 0px; display: inline-block; text-align: center; width: 250px;"><div style="display: inline-block; padding: 2px 4px; margin: 0px 0px 5px; border: 1px solid rgb(204, 204, 204); text-align: center; background-color: rgb(255, 255, 255);"><a href="http://localtimes.info/Asia/Philippines/Manila/" style="text-decoration: none; font-size: 13px; color: rgb(0, 0, 0);"><img src="http://localtimes.info/images/countries/ph.png"="" border=0="" style="border:0;margin:0;padding:0"=""> Manila</a></div><script type="text/javascript" src="http://localtimes.info/clock.php?continent=Asia&country=Philippines&city=Manila&cp1_Hex=000000&cp2_Hex=FFFFFF&cp3_Hex=000000&fwdt=288&ham=0&hbg=0&hfg=0&sid=0&mon=0&wek=0&wkf=0&sep=0&widget_number=1014"></script></div>
-</div>
-    </header>
-<div class="row">
-      <div class="col-lg-3 col-xs-1">
-          <!-- small box -->
-          <div class="small-box bg-aqua">
-            <div class="inner">
-              <h3>150</h3>
-
-              <p>Logistics</p>
-            </div>
-            <div class="icon">
-              <i class="fa  fa-legal"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-green">
-            <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-              <p>Camp Management</p>
-            </div>
-            <div class="icon">
-              <i class="fa fa-home"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3>44</h3>
-
-              <p>Food and Non-Food</p>
-            </div>
-            <div class="icon">
-              <i class="fa fa-cutlery"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-
-          
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-aqua">
-            <div class="inner">
-              <h3>150</h3>
-
-              <p>Law and Order</p>
-            </div>
-            <div class="icon">
-              <i class="fa  fa-legal"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-green">
-            <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-              <p>Camp Management</p>
-            </div>
-            <div class="icon">
-              <i class="fa fa-home"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3>44</h3>
-
-              <p>Food and Non-Food</p>
-            </div>
-            <div class="icon">
-              <i class="fa fa-cutlery"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<header>
+  <div class="time">
+          <div style="margin: 15px 0px 0px; display: inline-block; text-align: center; width: 250px;"><div style="display: inline-block; padding: 2px 4px; margin: 0px 0px 5px; border: 1px solid rgb(204, 204, 204); text-align: center; background-color: rgb(255, 255, 255);"><a href="http://localtimes.info/Asia/Philippines/Manila/" style="text-decoration: none; font-size: 13px; color: rgb(0, 0, 0);"><img src="http://localtimes.info/images/countries/ph.png"="" border=0="" style="border:0;margin:0;padding:0"=""> Manila</a></div><script type="text/javascript" src="http://localtimes.info/clock.php?continent=Asia&country=Philippines&city=Manila&cp1_Hex=000000&cp2_Hex=FFFFFF&cp3_Hex=000000&fwdt=288&ham=0&hbg=0&hfg=0&sid=0&mon=0&wek=0&wkf=0&sep=0&widget_number=1014"></script></div>
+  </div>  
+</header>
+  <div class="row">
+ <div class="col-lg-1 col-xs-6">
+  </div> 
+      <div class="col-lg-3 col-xs-6">
           <!-- small box -->
           <div class="small-box bg-red">
             <div class="inner">
-              <h3>65</h3>
+              <h3><sup style="font-size: 20px">Law and Order</sup></h3>
 
-              <p>Dead and Missing</p>
+              <p><sup style="font-size: 13px">last update</sup></p>
             </div>
             <div class="icon">
-              <i class="fa fa-users"></i>
+              <i class="fa  fa-legal"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="#" class="small-box-footer" data-toggle="modal" data-target="#lawOrder">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
-      </div>          
+
+<!--Modal-->
+    <div class="modal fade" id="lawOrder" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Law and Order</h4>
+        </div>
+        <div class="modal-body">
+
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-blue">
+            <div class="inner">
+              <h3><sup style="font-size: 20px">Camp Management</sup></h3>
+
+               <p><sup style="font-size: 13px">last update</sup></p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-home"></i>
+            </div>
+            <a href="#" class="small-box-footer" data-toggle="modal" data-target="#camp">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+<!--Modal-->
+    <div class="modal fade" id="camp" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Camp Coordianation and Management</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+            <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red-active">
+            <div class="inner">
+              <h3><sup style="font-size: 20px">Dead and Missing</sup></h3>
+
+              <p><sup style="font-size: 13px">last update</sup></p>
+            </div>
+            <div class="icon">
+              <i class="fa  fa-users"></i>
+            </div>
+            <a href="#" class="small-box-footer" data-toggle="modal" data-target="#deadM">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+<!--Modal-->
+    <div class="modal fade" id="deadM" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Dead and Missing</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-teal">
+            <div class="inner">
+              <h3> <sup style="font-size: 20px">Food and Non-Food</sup></h3>
+
+               <p><sup style="font-size: 13px">last update</sup></p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-cutlery"></i>
+            </div>
+            <a href="#" class="small-box-footer" data-toggle="modal" data-target="#food">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+<!--Modal-->
+    <div class="modal fade" id="food" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Food and Non-Food</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>     
+        <!---====-->
+        
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3><sup style="font-size: 20px">Logistics</sup></h3>
+
+              <p><sup style="font-size: 13px">last update</sup></p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-ambulance"></i>
+            </div>
+            <a href="#" class="small-box-footer" data-toggle="modal" data-target="#logistics">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+<!--Modal-->
+    <div class="modal fade" id="logistics" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Logistics</h4>
+        </div>
+        <div class="modal-body">
+          
+<p align="justify" style="text-indent: 50px"> Disasters are emergencies that cannot be handled by those affected without assistance. They are caused by natural or man-made events wherein communities experience severe danger and incur loss of lives and properties causing disruption to their social structure and to all or some of the affected communities’ essential functions. They are caused by unsustainable development that has not taken account of possible hazard impacts in that location.  They can be less damaging if there will be system that can help the management in faster administering and management of different resources.  </p>
+
+<p align="justify" style="text-indent: 50px"> Now, in disaster, logistics is one of the most significant and complex component of the disaster operations whether if it is in preparedness, response or recovery phases. Logistics deliver the appropriate supplies in good condition, in the quantities required and at the places and time they are needed. So, logistics must be both agile and flexible in times of disaster and in preparing needed supplies. </p>
+
+<p align="justify" style="text-indent: 50px"> Achieving those tasks of logistics is important because it can save and preserve millions and millions of lives in the disaster, and managing those are not easy and there is a need for a new, developed system that can handle those tasks and integrate different systems un users in just one click away.  </p>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-orange">
+            <div class="inner">
+              <h3><sup style="font-size: 20px">Inventory</sup></h3>
+
+              <p><sup style="font-size: 13px">last update</sup></p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-archive"></i>
+            </div>
+            <a href="#" class="small-box-footer" data-toggle="modal" data-target="#inv">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+<!--Modal-->
+    <div class="modal fade" id="inv" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Inventory</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3><sup style="font-size: 20px">Procurement</sup></h3>
+
+              <p><sup style="font-size: 13px">last update</sup></p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-money"></i>
+            </div>
+            <a href="#" class="small-box-footer" data-toggle="modal" data-target="#proc">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+<!--Modal-->
+    <div class="modal fade" id="proc" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Procurement</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+        </div>
+       
 
     <div class="body-content">
 
