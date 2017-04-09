7159d22b693f4df45a30f330c060ca3d5c0718a5
airacarpio <airajoyce04@gmail.com>
2017-04-09 16:39:27 +0800

    Updated Organizational Structure

--- GUI/monitoringv2/frontend/controllers/SiteController.php
+++ GUI/monitoringv2/frontend/controllers/SiteController.php
@@ -26,15 +26,15 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::className(),
-                'only' => ['logout', 'signup'],
+                'only' => ['logout', 'signup','login'],
                 'rules' => [
                     [
-                        'actions' => ['signup'],
+                        'actions' => ['login','error','signup'],
                         'allow' => true,
                         'roles' => ['?'],
                     ],
                     [
-                        'actions' => ['logout'],
+                        'actions' => ['logout','index'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
@@ -43,7 +43,6 @@ class SiteController extends Controller
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
-                    'logout' => ['post'],
                 ],
             ],
         ];
